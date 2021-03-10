93aece4f9988e32e0e7765f04b44f36fd1ea3f53
marco sartori <marcosartori98@yahoo.com>
2021-03-10 15:10:07 +0100

    aggiunta immagini

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-  header
+  <div class="logo">
+    <img src="{{ asset('img/logo.png')}} " alt="logo immagine">
+  </div>
+  <div class="menu">
+    <nav>
+      <ul>
+        <li><a href="{{ url('/') }}">Home</a></li>
+        <li><a href="">Prodotti</a></li>
+        <li><a href=" {{ url('news') }} ">News</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
