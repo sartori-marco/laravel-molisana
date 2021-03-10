629f5d877218e3f14fe202696f7722cc58ef1009
marco sartori <marcosartori98@yahoo.com>
2021-03-10 16:26:21 +0100

    vista prodotti

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-  <p>codice prodotti</p>
+  <!-- <p>codice prodotti</p> -->
+  <div class="container">
+    <div class="card-container">
+        @foreach($tipologie as $tipologia)
+      <div class="card">
+          <img src="{{$tipologia['src']}}" alt="">
+        <div class="overlay">
+          <a href="#">{{ $tipologia['titolo'] }}</a>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </div>
+
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -140,7 +140,8 @@ Route::get('prodotti', function () {
             "descrizione"=> "Altro elemento cult della famiglia de lo Spaghetto Quadrato (N.1 Spaghetto Quadrato. Una new entry che sarà molto apprezzata sia dai consumatori che dagli chef, perché il Ditale Quadrato è un formato deliziosamente piccolo ma sostanzioso.<br>A dispetto del nome che fa pensare ad una pastina è un formato di pasta assolutamente versatile, adatto a moltissime ricette di primi piatti.<br>La sua consistenza soda si sprigiona in bocca con un\'esplosione di emozioni, grazie agli spessori corposi, al colore elegantemente ambrato, alla texture delicatamente ruvida, cangiante e piacevolissima al tatto che trattiene il condimento sulla superficie.<br>Il Ditale Quadrato sembra ideale per preparazioni strutturate come la ricetta con crema di broccoletto siciliano, calamari e pomodori semi secchi profumata al limone e carbone d\'olive nere."
             ]
          ];
-    return view('prodotti');
+         $data = ['tipologie' => $pasta];
+    return view('prodotti', $data);
 });
 
 Route::get('news', function () {
