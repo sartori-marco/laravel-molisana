ff21f76a96dd10da0f6b3636d1451c62553cc114
marco sartori <marcosartori98@yahoo.com>
2021-03-11 16:50:49 +0100

    aggiunta icone

--- resources/views/dettagli.blade.php
+++ resources/views/dettagli.blade.php
@@ -3,6 +3,11 @@
 {{-- sistemare il nome che non si vede --}}
 @section('title',$formato['titolo'])    
 
+{{-- cdn icone --}}
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+@endsection
+
 @section('content')
     <div class="container">
         <h1>{{$formato['titolo']}}</h1>    
@@ -15,6 +20,20 @@
         {{-- visualizzo la descrizione uso !! per vedere i br che ho del database pasta--}} 
         <div class="descrizione">
             <p>{!!$formato['descrizione']!!}</p>
+        </div>
+        <div class="box-icone">
+            <div class="icone">
+                <i class="fas fa-info"></i>
+                <p>{!!$formato['tipo']!!}</p>
+            </div>
+            <div class="icone">
+                <i class="fas fa-clock"></i>
+                <p>{!!$formato['cottura']!!}</p>
+            </div>
+            <div class="icone">
+                <<i class="fas fa-weight-hanging"></i>
+                <p>{!!$formato['peso']!!}</p>
+            </div>
         </div>
     </div>
     {{-- /container --}}
