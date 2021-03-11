d560c2ad4354205a343111f8b0a6e24e29169ba0
marco sartori <marcosartori98@yahoo.com>
2021-03-11 16:33:04 +0100

    aggiunta descrizione

--- resources/views/dettagli.blade.php
+++ resources/views/dettagli.blade.php
@@ -6,12 +6,17 @@
 @section('content')
     <div class="container">
         <h1>{{$formato['titolo']}}</h1>    
+        <div class="prodotti">
+            {{-- visualizzo le immagini --}}
+            <img src="{{$formato['src-h']}}" alt="immagine pasta">
+            <img src="{{$formato['src-p']}}" alt="immagine pasta">   
+        </div>
+        {{-- /prodotti --}}
+        {{-- visualizzo la descrizione uso !! per vedere i br che ho del database pasta--}} 
+        <div class="descrizione">
+            <p>{!!$formato['descrizione']!!}</p>
+        </div>
     </div>
-
-    <div class="prodotti">
-        <img src="{{$formato['src-h']}}" alt="immagine pasta">
-        <img src="{{$formato['src-p']}}" alt="immagine pasta">
-    </div>
-
+    {{-- /container --}}
 
 @endsection
